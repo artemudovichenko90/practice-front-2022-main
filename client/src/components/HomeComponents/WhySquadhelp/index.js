463c3335b357b606f5be32b3e55a54dd86905545
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-08-10 18:06:00 +0300

    Add WhySquadhelp

---
+++ client/src/components/HomeComponents/WhySquadhelp/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CONSTANTS from '../../../constants';
+import styles from './WhySquadhelp.module.sass';
+import articles from './whySquadhelp.json';
+const WhySquadhelp = () => {
+  const showArticle = ({ img, alt, heading, content }, i) => (
+    <article className={styles.card}>
+      <img src={`${CONSTANTS.STATIC_IMAGES_PATH}${img}`} alt={alt} />
+      <h3>{heading}</h3>
+      <p>{content}</p>
+    </article>
+  )
+  return (
+    <section className={styles.container__description}>
+      <h2 className={styles.blueUnderline}>Why Squadhelp?</h2>
+      <div className={styles.cardContainer}>{articles.map(showArticle)}</div>
+    </section>
+  );
+}
+
+export default WhySquadhelp;
